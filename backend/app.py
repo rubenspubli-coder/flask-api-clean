4ea3d0d578fdfae2c78a5c1fda9279a49b54d291
rubenspubli-coder <rubenspubli@gmail.com>
2026-04-21 23:27:15 -0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ import os
 app = Flask(__name__, static_folder="../frontend", static_url_path="")
 
 @app.route("/")
-def home():
+def serve_frontend():
     return send_from_directory(app.static_folder, "index.html")
 
 @app.route("/api/hello")
