0e2b1ad1dfac1a955a0b1c3822bc5331c27217f7
rubenspubli-coder <rubenspubli@gmail.com>
2026-04-21 23:39:47 -0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,12 @@
 import os
 from flask import Flask, jsonify, send_file
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-FRONTEND_PATH = os.path.join(BASE_DIR, "..", "frontend", "index.html")
-
 app = Flask(__name__)
 
 @app.route("/")
 def serve_frontend():
-    return send_file(FRONTEND_PATH)
+    path = os.path.abspath(os.path.join(os.getcwd(), "..", "frontend", "index.html"))
+    return send_file(path)
 
 @app.route("/api/hello")
 def hello():
