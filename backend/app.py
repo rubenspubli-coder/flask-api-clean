885c627e2321caafbcfdfa9e1a2f81f4e016d4cf
rubenspubli-coder <rubenspubli@gmail.com>
2026-04-21 22:37:43 -0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,11 @@
+from flask import Flask, jsonify
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "ok"
+
+@app.route("/api/hello")
+def hello():
+    return jsonify({"message": "hello"})
